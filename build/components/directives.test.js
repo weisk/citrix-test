f9561859fd8cde9df5a1db032002664203f614d0
Francesc Mallafre Lopez <franml87@gmail.com>
2015-01-08 19:45:24 +0100

    Add built application

---
+++ build/components/directives.test.js
@@ -0,0 +1,6 @@
+'test of service defined in app_service goes here';
+describe('app_service_test', function() {
+  return it('should assert something', function() {
+    return expect(true).toBe(true);
+  });
+});
---
+++ build/main/main.test.js
@@ -0,0 +1,5 @@
+describe('main_controller_test', function() {
+  return it('should assert something', function() {
+    return expect(true).toBe(true);
+  });
+});
